2adb62603679b63d7d379cd9f9d4cbbaca56ab2a
Geniedev1 <MinhDN.B22CN528@stu.ptit.edu.vn>
2025-02-24 21:46:07 +0700

    Add css for page blog

--- Client/src/Pages/Blog.jsx
+++ Client/src/Pages/Blog.jsx
@@ -6,12 +6,28 @@ import { ListProduct } from '../Component/HomeComponent/ListProduct'
 export const Blog = () => {
   useEffect(() => { window.scrollTo(0, 0) }, []);
 
-
   return (
     <>
-    <Header/>
-    <ListProduct/>
-    <Footer/>
+      <Header />
+      <main className="blog-main">
+        <section className="blog-content">
+          <h1>Blog</h1>
+          <p>Welcome to our blog! Here you will find the latest updates and articles.</p>
+          <article className="blog-post">
+            <h2>Blog Post Title</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </article>
+          <article className="blog-post">
+            <h2>Another Blog Post</h2>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </article>
+        </section>
+        <aside className="sidebar">
+          <h2>Related Products</h2>
+          <ListProduct />
+        </aside>
+      </main>
+      <Footer />
     </>
   )
 }
