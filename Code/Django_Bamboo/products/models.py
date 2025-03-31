ce1d69d1083b8595e670fd2541cd826b56baaba0
Geniedev1 <MinhDN.B22CN528@stu.ptit.edu.vn>
2025-03-31 22:25:07 +0700

    contact

--- Code/Django_Bamboo/products/models.py
+++ Code/Django_Bamboo/products/models.py
@@ -12,6 +12,7 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10)
     description = models.TextField(blank=True, null=True)
     image_url = models.URLField(max_length=500, blank=True, null=True)
+    image_url2 = models.URLField(max_length=500, blank=True, null=True)
     categories = models.ManyToManyField(Category, related_name='products')
 
     def __str__(self):
